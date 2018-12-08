b46d9577f032e6f495981609686234d0df93047c
reo <reo@lezhin.com>
2018-12-08 11:31:55 +0900

    create kakao login

--- Sooling-iOS/Sooling-iOS/View/ViewController.swift
+++ Sooling-iOS/Sooling-iOS/View/ViewController.swift
@@ -7,9 +7,41 @@
 //
 
 import UIKit
+import KakaoOpenSDK
 
 class ViewController: UIViewController {
 
+//    let buttton = KOLoginButton.
+    
+    @IBAction func kakaoButton(_ sender: Any) {
+        let session = KOSession.shared()
+        
+        if let s = session {
+            if s.isOpen() {
+                s.close()
+            }
+            
+            s.open(completionHandler: { error in
+                if error == nil {
+                    print("no error")
+                    
+                    if s.isOpen() {
+                        KOSessionTask.userMeTask(completion: { error, file in
+                            print("errorL: \(error)")
+                            print("file: \(file)")
+                        })
+                    } else {
+                        print("실패")
+                    }
+                } else {
+                    print("login error")
+                }
+            })
+        }else {
+            print("세션 에러")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
